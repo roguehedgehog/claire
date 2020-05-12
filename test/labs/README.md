fa84cff2cdb921887e7cb706b51fbf17bb31222b
Tahir Bulbrook <t@hir.me.uk>
2020-05-12 20:42:50 +0100

    Create instance and connect using local key

--- test/labs/README.md
+++ test/labs/README.md
@@ -1,58 +1,17 @@
 
-# Welcome to your CDK Python project!
+# Welcome to CLAIRE Labs!
 
-This is a blank project for Python development with CDK.
+## Getting Started
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
-
-This project is set up like a standard Python project.  The initialization
-process also creates a virtualenv within this project, stored under the .env
-directory.  To create the virtualenv it assumes that there is a `python3`
-(or `python` for Windows) executable in your path with access to the `venv`
-package. If for any reason the automatic creation of the virtualenv fails,
-you can create the virtualenv manually.
-
-To manually create a virtualenv on MacOS and Linux:
+Generate a keypair and add it to your AWS account
 
 ```
-$ python -m venv .env
-```
+ ssh-keygen -t rsa -C "claire-lab-key" -f ~/.ssh/claire-lab-key
+ aws ec2 import-key-pair --key-name "claire lab key" --public-key-material fileb://~/.ssh/claire-lab-key.pub
+ ```
 
-After the init process completes and the virtualenv is created, you can use the following
-step to activate your virtualenv.
+ Deploy the lab scope passing the name of the key which will be attached to the vulnerable instance
 
-```
-$ source .env/bin/activate
-```
-
-If you are a Windows platform, you would activate the virtualenv like this:
-
-```
-% .env\Scripts\activate.bat
-```
-
-Once the virtualenv is activated, you can install the required dependencies.
-
-```
-$ pip install -r requirements.txt
-```
-
-At this point you can now synthesize the CloudFormation template for this code.
-
-```
-$ cdk synth
-```
-
-To add additional dependencies, for example other CDK libraries, just add
-them to your `setup.py` file and rerun the `pip install -r requirements.txt`
-command.
-
-## Useful commands
-
- * `cdk ls`          list all stacks in the app
- * `cdk synth`       emits the synthesized CloudFormation template
- * `cdk deploy`      deploy this stack to your default AWS account/region
- * `cdk diff`        compare deployed stack with current state
- * `cdk docs`        open CDK documentation
-
-Enjoy!
+ ```
+ cdk deploy -c "lab-key=claire lab key"
+ ```
--- test/labs/labs/labs_stack.py
+++ test/labs/labs/labs_stack.py
@@ -1,9 +1,4 @@
-from aws_cdk import (
-    core,
-    aws_ec2 as ec2,
-    aws_kms as kms,
-    aws_rds as rds
-)
+from aws_cdk import core, aws_ec2 as ec2, aws_kms as kms, aws_rds as rds
 
 
 class LabsStack(core.Stack):
@@ -13,20 +8,47 @@ class LabsStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        vpc = ec2.Vpc(self, "VPC", max_azs=1)
-        key = kms.Key(self, "Key", removal_policy=core.RemovalPolicy.DESTROY)
-        sec_group = ec2.SecurityGroup(self, "SecurityGroup",
-                                      vpc=vpc,
-                                      allow_all_outbound=True
-                                      )
-        sec_group.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(22))
-        instance = ec2.Instance(self, "Instance",
-                                instance_type=ec2.InstanceType("m1.small"),
-                                machine_image=ec2.LookupMachineImage(
-                                    name="ubuntu-minimal/images/hvm-ssd/ubuntu-bionic-18.04-amd64-minimal-*",
-                                    owners=[self.CANONICAL_USER_ID]
-                                ),
-                                vpc=vpc,
-                                security_group=sec_group,
-                                key_name="Key"  
-                                )
+        vpc = self.create_vpc()
+        instance = self.create_ubuntu_instance(
+            vpc,
+            self.create_security_group(vpc),
+        )
+
+    def create_vpc(self) -> ec2.Vpc:
+        return ec2.Vpc(
+            self,
+            "VPC",
+            max_azs=1,
+            subnet_configuration=[
+                ec2.SubnetConfiguration(
+                    subnet_type=ec2.SubnetType.PUBLIC,
+                    name="Public",
+                    cidr_mask=24,
+                )
+            ],
+        )
+
+    def create_security_group(self, vpc: ec2.Vpc) -> ec2.SecurityGroup:
+        security_group = ec2.SecurityGroup(
+            self,
+            "SecurityGroup",
+            vpc=vpc,
+            allow_all_outbound=True,
+        )
+        security_group.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(22))
+
+        return security_group
+
+    def create_ubuntu_instance(self, vpc, security_group) -> ec2.Instance:
+        ec2.Instance(
+            self,
+            "Instance",
+            instance_type=ec2.InstanceType("m1.small"),
+            machine_image=ec2.LookupMachineImage(
+                name=
+                "ubuntu-minimal/images/hvm-ssd/ubuntu-bionic-18.04-amd64-minimal-*",
+                owners=[self.CANONICAL_USER_ID]),
+            vpc=vpc,
+            security_group=security_group,
+            key_name=self.node.try_get_context("lab_key"),
+        )
