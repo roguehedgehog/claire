a79a2b3eec2c83dd349f38ad8d0e24ab987cca7e
Tahir Bulbrook <t@hir.me.uk>
2020-07-25 10:19:33 +0100

    Move detele volume to volume state machine

--- tf/claire/state_machine.tf
+++ tf/claire/state_machine.tf
@@ -9,9 +9,6 @@ data "template_file" "claire_acquisition_state_machine" {
   template = file("state_machines/acquisition.json")
 
   vars = {
-    detach_memory_volume      = aws_lambda_function.detach_memory_volume.arn
-    is_memory_volume_detached = aws_lambda_function.is_memory_volume_detached.arn
-
     move_memory_volume           = aws_sfn_state_machine.claire_move_volume.arn
     create_investigation         = aws_lambda_function.create_investigation.arn
     create_evidence_extractor    = aws_lambda_function.create_evidence_extractor.arn
@@ -20,7 +17,6 @@ data "template_file" "claire_acquisition_state_machine" {
     capture_memory               = aws_lambda_function.capture_memory.arn
     upload_memory                = aws_lambda_function.upload_memory.arn
     is_command_complete          = aws_lambda_function.is_command_complete.arn
-    delete_memory_volume         = aws_lambda_function.delete_memory_volume.arn
     isolate_instance             = aws_lambda_function.isolate_instance.arn
     snapshot_disks               = aws_lambda_function.snapshot_disks.arn
     terminate_evidence_extractor = aws_lambda_function.terminate_evidence_extractor.arn
@@ -41,5 +37,6 @@ data "template_file" "claire_move_volume_state_machine" {
     is_memory_volume_detached = aws_lambda_function.is_memory_volume_detached.arn
     attach_memory_volume      = aws_lambda_function.attach_memory_volume.arn
     is_memory_volume_attached = aws_lambda_function.is_memory_volume_attached.arn
+    delete_memory_volume      = aws_lambda_function.delete_memory_volume.arn
   }
 }
