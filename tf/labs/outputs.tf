08c6aa408dd1985d53527124058767a26aff0f08
Tahir Bulbrook <t@hir.me.uk>
2020-08-17 10:26:45 +0100

    Improve formatting for multiple instances

--- tf/labs/outputs.tf
+++ tf/labs/outputs.tf
@@ -1,13 +1,13 @@
 output "lab_instance_id" {
-  value = join("", aws_instance.lab.*.id)
+  value = join(",", aws_instance.lab.*.id)
 }
 
 output "lab_public_dns" {
-  value = join("", aws_instance.lab.*.public_dns)
+  value = join(",", aws_instance.lab.*.public_dns)
 }
 
 output "lab_public_ip" {
-  value = join("", aws_instance.lab.*.public_ip)
+  value = join(",", aws_instance.lab.*.public_ip)
 }
 
 output "bucket_target" {
